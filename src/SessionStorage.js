343604db311ef2d620b5e0c16a3a06918d2ebd87
James Gillmore <james@faceyspacey.com>
2017-05-02 05:37:51 -0700

    fix($component): set correct key for <ScrollContainer /> saves in sessionStorage

--- src/SessionStorage.js
+++ src/SessionStorage.js
@@ -1,6 +1,12 @@
 const STATE_KEY_PREFIX = '@@scroll|'
 
+let prevKey = null
+
 export default class SessionStorage {
+  setPrevKey(key) {
+    prevKey = key
+  }
+
   read(location, key) {
     const stateKey = this.getStateKey(location, key)
     const value = sessionStorage.getItem(stateKey)
@@ -8,13 +14,24 @@ export default class SessionStorage {
   }
 
   save(location, key, value) {
+    if (key) {
+      location = { key: prevKey, hash: location.hash }
+    }
+
     const stateKey = this.getStateKey(location, key)
     const storedValue = JSON.stringify(value)
     sessionStorage.setItem(stateKey, storedValue)
+
+    if (key) {
+      const newKey = location.key || location.hash || 'loadPage'
+      if (newKey !== prevKey) {
+        prevKey = newKey
+      }
+    }
   }
 
   getStateKey(location, key) {
-    const locationKey = location.key || location.hash
+    const locationKey = location.key || location.hash || 'loadPage'
     const stateKeyBase = `${STATE_KEY_PREFIX}${locationKey}`
     return key == null ? stateKeyBase : `${stateKeyBase}|${key}`
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import ScrollBehavior from 'scroll-behavior'
 import SessionStorage from './SessionStorage'
 
-export default shouldUpdateScroll => history =>
-  new ScrollBehavior({
+export default shouldUpdateScroll => history => {
+  const stateStorage = new SessionStorage()
+
+  const behavior = new ScrollBehavior({
     addTransitionHook: history.listen,
-    stateStorage: new SessionStorage(),
+    stateStorage,
     getCurrentLocation: () => ({
       ...history.location,
       action: history.action
     }),
     shouldUpdateScroll
   })
+
+  behavior.setPrevKey = () => {
+    const key = history.location.key || history.location.hash || 'loadPage'
+    stateStorage.setPrevKey(key)
+  }
+
+  return behavior
+}
